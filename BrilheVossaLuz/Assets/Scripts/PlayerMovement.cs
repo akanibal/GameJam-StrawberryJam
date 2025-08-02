6149f1c1f009205ed3cdf3ccb51c5a62e63d8350
Marcosware <marcosware.dev@gmail.com>
2025-08-02 02:10:32 -0300

    feat: Player Sprite and Interaction Function

--- BrilheVossaLuz/Assets/Scripts/PlayerMovement.cs
+++ BrilheVossaLuz/Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,49 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     public CharacterController controller;
+    public InputActionReference interact;
+    public SpriteRenderer spriteRend;
+    public Sprite[] sprites;
     public float moveSpeed = 5f;
+    public float rotationSpeed = 50f;
+
     void Start()
     {
-
+        spriteRend = GetComponent<SpriteRenderer>();
     }
+
     void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Debug.Log("attacked!");
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log("interacted!");
+        }
+    }
+
+    void Rotate()
+    {
+        
+    }
+    void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
         Vector3 movement = new Vector3(x, 0f, z).normalized;
         controller.Move(movement * moveSpeed * Time.deltaTime);
+        if (movement.x == -1)
+        {
+            spriteRend.sprite = sprites[0];
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (movement.x == 1) spriteRend.sprite = sprites[1];
+        else if (movement.z == -1) spriteRend.sprite = sprites[2];
+        else if (movement.z == 1) spriteRend.sprite = sprites[3];
     }
 }
--- BrilheVossaLuz/Assets/Scripts/Task.cs
+++ BrilheVossaLuz/Assets/Scripts/Task.cs
@@ -1,17 +1,69 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class Task : MonoBehaviour
 {
-
-    public bool broken = false;
+    public MeshFilter meshFilter;
+    public Mesh brokenMesh;
+    public Mesh fixedMesh;
+    public bool broken;
+    public bool playerIn = false;
+    public bool enemyIn = false;
+    public int repairCount;
+    public int maxRepairCount;
 
     void Start()
     {
-        
+        meshFilter = GetComponent<MeshFilter>();
+        if (meshFilter != null)
+        {
+            fixedMesh = meshFilter.mesh;
+        }
+        Break();
     }
-    
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            if (playerIn) repairCount--;
+            if (repairCount == 0) Fix();
+        }
+    }
+
+    void Break()
+    {
+        repairCount = maxRepairCount;
+        broken = true;
+        meshFilter.mesh = brokenMesh;
+    }
+
+    void Fix()
+    {
+        broken = false;
+        Debug.Log("fixed!");
+        meshFilter.mesh = fixedMesh;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // 
+        if (other.gameObject.tag == "Player")
+        {
+            playerIn = true;
+            Debug.Log("player in!");
+        }
+        else if (other.gameObject.tag == "Enemy") enemyIn = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerIn = false;
+            Debug.Log("player out!");
+        }
+        else if (other.gameObject.tag == "Enemy") enemyIn = false;
     }
 }
