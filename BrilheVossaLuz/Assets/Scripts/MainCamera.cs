7c68de1b47eb83e30b409235da70acea8688881c
Marcosware <marcosware.dev@gmail.com>
2025-08-02 16:01:39 -0300

    Kitchen modelling and scenario

--- BrilheVossaLuz/Assets/Scripts/MainCamera.cs
+++ BrilheVossaLuz/Assets/Scripts/MainCamera.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class MainCamera : MonoBehaviour
 {
     public Vector3[] positions = {
-        new Vector3(-13.5f, 7f, 7.8f),
-        new Vector3(-13.5f, 9.5f, -2.4f),
+        new Vector3(-14f, 9.3f, 7f),
+        new Vector3(-13.75f, 10f, -1.8f),
         new Vector3(-6.25f, 14f, -7f)
     };
 
